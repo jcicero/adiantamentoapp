990d51e58fc955ce520d56edad0a2cb1e5d5417c
José Cícero Rocha Cavalcante <jcicero@gmail.com>
2020-09-04 22:27:57 -0300

    Commit inicial + listagem de processos

---
+++ src/pages/Processo.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-page padding>
+
+      <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card" v-for="(adiantamento, id) in processos" :key="id">
+         <q-card flat bordered class="my-card bg-grey-1">
+      <q-card-section>
+        <div class="row items-center no-wrap">
+          <div class="col">
+            <div class="text-h6">{{ adiantamento.processo }}</div>
+            <div class="text-subtitle2">{{ adiantamento.valor }}</div>
+            <div class="text-subtitle2">Liberação: {{ adiantamento.data }}</div>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="text-subtitle2">{{ adiantamento.responsavel }}</div>
+        <div class="text-subtitle2">{{ adiantamento.status }}</div>
+      </q-card-section>
+
+      <q-separator />
+
+    </q-card>
+          <q-card-actions align="right">
+            <q-btn flat round color="blue" icon="remove_red_eye" />
+            <q-btn flat round color="teal" icon="edit" />
+            <q-btn flat round color="red" icon="delete" />
+          </q-card-actions>
+        </q-card>
+      </div>
+    <p >
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Processo',
+  data () {
+    return {
+      processos: []
+    }
+  },
+  mounted () {
+    this.getProcessos()
+  },
+  methods: {
+    getProcessos () {
+      this.$axios.get('http://adiantamento.test/api/adiantamento')
+        .then((res) => {
+          this.processos = res.data.data
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 300px
+</style>
