456e71a84e289b7e21d70c6db4e168e0047d1fd3
José Cícero Rocha Cavalcante <jcicero@gmail.com>
2020-09-16 23:36:09 -0300

    Notify NF cadastrada co msucesso

--- src/pages/Adiantamento.vue
+++ src/pages/Adiantamento.vue
@@ -1,5 +1,13 @@
 <template>
   <q-page padding>
+    <q-card v-if="showcard" class="my-card">
+      <q-card-section>
+        <b>Dados da Nota/Cupom Fiscal</b>
+      </q-card-section>
+
+        <q-separator />
+
+      <q-card-section>
     <q-select
       outlined
       v-model="danfe.fornecedor_id"
@@ -51,17 +59,94 @@
       </div>
     </div>
 
-    <q-input
-      outlined
-      v-model="danfe.user_id"
-      label="Usuario"
-    />
+      </q-card-section>
+
+        <q-separator />
+
+      <q-card-section>
+        <b>Incluir itens da Nota/Cupom Fiscal</b>
+      </q-card-section>
+        <q-separator />
+
+      <q-card-section>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-6">
+          <q-input
+            outlined
+            v-model="danfe.nf"
+            label="Descrição do produto"
+          />
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-2">
+          <q-input
+            v-model.number="danfe.nf"
+            type="number"
+            outlined
+            label="Quantidade"
+          />
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-2">
+          <q-input
+            v-model.number="danfe.nf"
+            type="number"
+            outlined
+            label="Valor Total"
+          />
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-1">
+      <q-field
+        outlined
+        label="R$ Unitário"
+        stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0"></div>
+        </template>
+      </q-field>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-1">
+        <q-btn
+          size="16px"
+          round
+          color="primary"
+          icon="add"
+        />
+      </div>
+    </div>
+      </q-card-section>
+      <q-markup-table>
+        <thead>
+          <tr>
+            <th class="text-left">#</th>
+            <th class="text-left">Descrição</th>
+            <th class="text-right">Quantidade</th>
+            <th class="text-right">Valor Unitário</th>
+            <th class="text-right">Valor Total</th>
+            <th class="text-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="text-left">1</td>
+            <td class="text-left">Frozen Yogurt</td>
+            <td class="text-right">10</td>
+            <td class="text-right">6</td>
+            <td class="text-right">60</td>
+            <td class="text-center">X</td>
+          </tr>
+        </tbody>
+      </q-markup-table>
+
+    <q-separator />
+    <q-card-actions>
       <q-btn
         outlined
         color="primary"
         label="Salvar"
         @click.stop.prevent="postDanfe()"
       />
+    </q-card-actions>
+    </q-card>
+
     <table-list v-if="renderComponent"
       :titulo="titulo"
       :data="data"
@@ -69,6 +154,10 @@
       :filter="filter"
       :valorTotal="valorTotal"
     />
+
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn fab icon="add" color="primary" @click.stop.prevent="showcard = true" />
+    </q-page-sticky>
   </q-page>
 </template>
 
@@ -86,6 +175,7 @@ export default {
       titulo: 'Notas Fiscais',
       renderComponent: true,
       danfe: [],
+      showcard: false,
       data: [
         {
           nf: '',
@@ -142,7 +232,7 @@ export default {
           valor: this.danfe.valor,
           fornecedor_id: this.danfe.fornecedor_id,
           adiantamento_id: this.$route.params.id,
-          user_id: this.danfe.user_id
+          user_id: '1'
         })
         .then(res => {
           this.danfe.nf = ''
@@ -150,7 +240,9 @@ export default {
           this.danfe.valor = ''
           this.danfe.fornecedor_id = ''
           this.danfe.user_id = ''
+          this.showcard = false
           this.forceRerender()
+          this.triggerPositive()
         })
         .catch(err => {
           console.error(err)
@@ -185,6 +277,13 @@ export default {
       this.$nextTick(() => {
         this.renderComponent = true
         this.getDanfe()
+      })
+    },
+
+    triggerPositive () {
+      this.$q.notify({
+        type: 'positive',
+        message: 'Cadastrado com sucesso.'
       })
     }
   }
