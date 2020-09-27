46da5f3e6772462d6d0bcea4fa37265758cb24b3
José Cícero Rocha Cavalcante <jcicero@gmail.com>
2020-09-27 01:31:53 -0300

    Incluir itens em array

--- src/components/FormDanfe.vue
+++ src/components/FormDanfe.vue
@@ -86,13 +86,13 @@
       <div class="col-xs-12 col-sm-6 col-md-6">
           <q-input
             outlined
-            v-model="danfe.nf"
+            v-model="descricaoField"
             label="Descrição do produto"
           />
       </div>
       <div class="col-xs-12 col-sm-6 col-md-2">
           <q-input
-            v-model.number="danfe.nf"
+            v-model.number="quantidadeField"
             type="number"
             outlined
             label="Quantidade"
@@ -100,7 +100,7 @@
       </div>
       <div class="col-xs-12 col-sm-6 col-md-2">
           <q-input
-            v-model.number="danfe.nf"
+            v-model.number="valorField"
             type="number"
             outlined
             label="Valor Total"
@@ -112,42 +112,28 @@
         label="R$ Unitário"
         stack-label>
         <template v-slot:control>
-          <div class="self-center full-width no-outline" tabindex="0"></div>
+          <div class="self-center full-width no-outline" tabindex="0">{{ calcUnitario }}</div>
         </template>
       </q-field>
       </div>
       <div class="col-xs-12 col-sm-6 col-md-1">
         <q-btn
-          size="16px"
+          size="14px"
           round
           color="primary"
           icon="add"
+          @click.stop.prevent="addProdutos"
         />
       </div>
     </div>
+      <div class="q-pa-md">
+        <q-table dense
+          :data="produtos"
+          :columns="columns"
+          row-key="name"
+        />
+      </div>
       </q-card-section>
-      <q-markup-table>
-        <thead>
-          <tr>
-            <th class="text-left">#</th>
-            <th class="text-left">Descrição</th>
-            <th class="text-right">Quantidade</th>
-            <th class="text-right">Valor Unitário</th>
-            <th class="text-right">Valor Total</th>
-            <th class="text-center">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="text-left">1</td>
-            <td class="text-left">Frozen Yogurt</td>
-            <td class="text-right">10</td>
-            <td class="text-right">6</td>
-            <td class="text-right">60</td>
-            <td class="text-center">X</td>
-          </tr>
-        </tbody>
-      </q-markup-table>
 
     <q-separator />
     <q-card-actions>
@@ -173,12 +159,64 @@ export default {
   data () {
     return {
       danfe: [],
-      fornecedors: []
+      fornecedors: [],
+      produtos: [],
+      descricaoField: '',
+      quantidadeField: 0,
+      valorField: 0,
+      numero: 1,
+      columns: [
+        {
+          name: 'numero',
+          align: 'center',
+          label: '#',
+          field: 'numero',
+          sortable: true
+        },
+        {
+          name: 'descricao',
+          align: 'center',
+          label: 'Descrição',
+          field: 'descricao',
+          sortable: true
+        },
+        {
+          name: 'quantidade',
+          align: 'center',
+          label: 'Quantidade',
+          field: 'quantidade',
+          sortable: true
+        },
+        {
+          name: 'valor',
+          align: 'center',
+          field: 'valor',
+          label: 'Valor',
+          sortable: true
+        },
+        {
+          name: 'unitario',
+          align: 'center',
+          field: 'unitario',
+          label: 'Valor Unitário',
+          sortable: true
+        }
+      ]
     }
   },
 
   mounted () {
     this.getFornecedor()
+  },
+
+  computed: {
+    calcUnitario () {
+      if (this.valorField > 0 && this.quantidadeField > 0) {
+        return (this.valorField / this.quantidadeField).toFixed(2)
+      } else {
+        return 0
+      }
+    }
   },
 
   methods: {
@@ -231,6 +269,21 @@ export default {
         const needle = val.toLowerCase()
         this.fornecedors = this.fornecedor.filter(v => v.nome.toLowerCase().indexOf(needle) > -1)
       })
+    },
+
+    addProdutos () {
+      console.log(this.produtos)
+      this.produtos.unshift({
+        numero: this.numero++,
+        descricao: this.descricaoField,
+        quantidade: this.quantidadeField,
+        valor: this.valorField,
+        unitario: this.calcUnitario,
+        id: Date.now()
+      })
+      this.descricaoField = ''
+      this.quantidadeField = ''
+      this.valorField = ''
     }
   }
 }
